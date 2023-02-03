db2653a34266edfead7c03c0ee177315b71265ae
dericksaa <dmsaao@unal.edu.co>
2023-02-03 13:25:11 -0500

    first pull request

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const dotEnv = require('dotenv')
 const app = express()
 const port = 5000
+dotEnv.config()
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.listen(port, _ => {
     console.log(`Server running in port ${port}`)
+    console.log(process.env.URL_DB);
 })
+
