f96fc722d434d6ec5124ea163008c29d490a4987
mardelys <mardelys1996@gmail.com>
2023-02-08 18:02:31 -0500

    jwt

--- controller/user.js
+++ controller/user.js
@@ -1,22 +1,28 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const getUser = async (req, res) => await User.find()
 
 
-const saveUser = async (req, res) => {
-   console.log(req.body)
-   try {
-      const { body } = req
-      const user = new User(body)
-      const usersave = await user.save()
-      return res.status(200).json(usersave)
 
-   } catch (error) {
-      console.log(error)
-      return res.status(400).json({
-         msg: `Error: ${error.message}`
-      })
-   }
+const saveUser = async (req, res) => {
+
+   const { email, password, role } = req.body;
+
+   const newUser = new User({
+      email,
+      password: await User.encryptPassword(password),
+      role
+   })
+   const savedU = await newUser.save();
+   console.log(newUser);
+  //usamos el metodo para crear el token y le pasamos los parametros que utilizará para crear el token
+  //palabra secreta, se recomienda crear un archivo config donde se exporte un objeto llamado SECRET: 'palabra' y se importa ese archivo y se usa config.SECRET en vez de lo que esta entre comillas simples
+   const token = jwt.sign({id: savedU._id}, 'user.api', {
+      expiresIn: 86400 //24h
+   })
+
+   res.status(200).json({token})
 }
 
 const updateUser = async (req, res) => await User.findByIdAndUpdate(id)
@@ -28,7 +34,7 @@ const logInUser = async (req, res) => {
 
 }
 
-module.exports ={
+module.exports = {
    getUser,
    saveUser,
    updateUser,
--- models/User.js
+++ models/User.js
@@ -1,22 +1,40 @@
 const {Schema, model} = require("mongoose")
+const bcrypt = require("bcryptjs")
 
-const UserSchema = Schema({
+
+const UserSchema = new Schema({
    email: {
       type: String,
-      required: true,
+      required: [true, "El correo es necesario"]
    },
    password: {
       type: String,
-      required: true,
+      required: [true, "La contraseña es obligatoria"]
    },
    role: {
       type: String,
-      default: 'user',
+    default: 'USER',
    }
 },
 {
    versionKey: false,
 }
-)
+);
+//los statics son formas de llamar metodos estaticos
+UserSchema.statics.encryptPassword = async (password) => {
+//metodo para aplicar un algoritmo especificando el recorrido
+const salt = await bcrypt.genSalt(10)
+//retornamos una contraseña ya cifrada, el metodo hash
+return await bcrypt.hash(password, salt)
+}
+
+
+
+
+//nueva contraseña que el usuario quiere comparar para ver si es falsa o no
+UserSchema.statics.comparePassword = async (password, receivePassword) => {
+   //retorna un booleano si conincide:true si no: false
+   return await bcrypt.compare(password, receivePassword)
+}
 
 module.exports = model("User", UserSchema)
