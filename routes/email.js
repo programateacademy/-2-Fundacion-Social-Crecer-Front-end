e573dd40324041cff78a7c155c8f7439eec8da6e
ElPardo16 <andresito105@gmail.com>
2023-03-09 13:30:14 -0500

    delete backend

--- routes/email.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { postEmail } = require("../controller/email");
-
-const routerEmail = Router()
-
-routerEmail.post('/send', postEmail)
-
-module.exports = routerEmail
